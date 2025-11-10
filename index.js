41eb0ef090bf03ebbf83709e1497b8617483998a
Dirk Arnez <smalldirkalex@gmail.com>
2025-11-11 03:35:51 +0800

    - update index.js

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ import * as e from  "yeoman-environment";
 import fs from "node:fs";
 
 const env = e.createEnv();
-
 env.lookup()
 .then((app) => {
     console.log(JSON.stringify(app));
     fs.mkdirSync("dist");
     t.default.create(app.find(a => a.namespace == "cpp-github:app").resolved, {cwd: "dist"}, {})
-    .withAnswers({
+    .withAnswers(/*{
         ProjectName: "test-application",
         ProjectNamePretty: "Test C++ Application",
         ProjectDescription: "Dummy description for the application",
         ProjectWebPage: "http://example.com",
         ProjectMaintainersEmail: "tester@testerenko.com",
-    })
+    }*/JSON.parse(fs.readFileSync('./prompt-answer.json', 'utf8')))
     .then((a) => {
         console.log("done");
     })
 });
+
 
 
 
