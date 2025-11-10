01b6c0e6ba739c4d10f05a1cd59c0f7d0482d502
Dirk Arnez <smalldirkalex@gmail.com>
2025-11-10 17:43:41 +0800

    - upload files

---
+++ index.js
@@ -0,0 +1,14 @@
+// import * as t from  "yeoman-test";
+// import path from "node:path";
+
+(async function () {
+      const { YeomanTest } = await import("yeoman-test");
+      const yeomanTest = new YeomanTest().create("cpp-github", {}, {}).withAnswers({
+        ProjectName: "test-application",
+        ProjectNamePretty: "Test C++ Application",
+        ProjectDescription: "Dummy description for the application",
+        ProjectWebPage: "http://example.com",
+        ProjectMaintainersEmail: "tester@testerenko.com",
+      });
+      await yeomanTest.run();
+})();
