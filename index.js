17dd47f95de5bdb62d0fc7b15c1240dc1c478b85
Dirk Arnez <smalldirkalex@gmail.com>
2025-11-11 15:43:58 +0800

    - update index.js

--- index.js
+++ index.js
@@ -6,19 +6,21 @@ const env = e.createEnv();
 env.lookup()
 .then((app) => {
     console.log(JSON.stringify(app));
+    const answers = JSON.parse(fs.readFileSync('./prompt-answer.json', 'utf8')))
     fs.mkdirSync("dist");
     t.default.create(app.find(a => a.namespace == "cpp-github:app").resolved, {cwd: "dist"}, {})
-    .withAnswers(/*{
+    .withAnswers(answers/*{
         ProjectName: "test-application",
         ProjectNamePretty: "Test C++ Application",
         ProjectDescription: "Dummy description for the application",
         ProjectWebPage: "http://example.com",
         ProjectMaintainersEmail: "tester@testerenko.com",
-    }*/JSON.parse(fs.readFileSync('./prompt-answer.json', 'utf8')))
+    }*/
     .then((a) => {
         console.log("done");
     })
 });
+
 
 
 
